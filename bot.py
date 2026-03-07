10b1660112c61866f1946470ce42241fafa78396
Sander <sander.schaass@gmail.com>
2026-03-07 22:05:42 +0100

    F

--- bot.py
+++ bot.py
@@ -136,7 +136,7 @@ async def sotw(ctx, dur: int):
             "**WebThePanda (Owner): 0** votes\n"
             "**Cats (Cool Guy): 0** votes\n"
             "**Bamboot (Bot): 0** votes\n\n"
-            "<t:{endTimeUnix}:R>"
+            "Time Remaining: <t:{endTimeUnix}:R>"
         ),
         colour=discord.Color.blurple()
     )
