51f030cc926e9a9d8274fe73e942f88aac6148ed
Sander <sander.schaass@gmail.com>
2026-03-08 14:00:57 +0100

    u

--- bot.py
+++ bot.py
@@ -67,7 +67,7 @@ async def on_member_join(member):
 
 @bot.command(name="test")
 async def test(ctx):
-    await ctx.send_message(f"<@&1049064880315052032>")
+    await ctx.send(f"<@&1049064880315052032>")
 
 
 bot.run(token)
